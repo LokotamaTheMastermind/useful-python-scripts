8e4da649377c19ff8548d0fc087e9f6383e50000
LokotamaTheMastermind <57059138+LokotamaTheMastermind@users.noreply.github.com>
2020-08-21 09:11:27 +0100

    Added stuff
    Added simple-interest-calculator | Added compound-interest-calculator (BETA) | Added word-matcher

---
+++ compound-interest-calc.py
@@ -0,0 +1,8 @@
+value_1 = input("Please what is the intial principal ->₦")
+value_2 = input("Please what is the rate ->%")
+value_3 = int(input("How long do you want to watch the principal ->yrs"))
+
+def calculate(principal, rate, time):
+    pass
+
+calculate(value_1, value_2, value_3)
---
+++ simple-interest-calc.py
@@ -0,0 +1,36 @@
+""" Comments
+
+Formula -> (Simple Interest)
+----------------------------
+
+P * R * T
+---------
+   100
+
+"""
+
+print("")
+value_1 = int(input("Please what is the principal ->₦"))
+print("")
+value_2 = int(input("Please what is the rate ->%"))
+print("")
+value_3 = int(input("Please what is the time ->yrs"))
+
+
+def calculate(principal, rate, time):
+    print("")
+
+    interest = principal * rate * time / 100
+
+    print(f"The simple interest is ->₦{interest}")
+
+    question = input("Do you want to find the amount? |> ")
+
+    if question == "y" or "yes":
+        amount = principal - interest
+        print("")
+        print(f"The amount of the simple interest is ->₦{amount}")
+    else:
+        print("")
+
+calculate(value_1, value_2, value_3)
