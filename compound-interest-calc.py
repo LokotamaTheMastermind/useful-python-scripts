f6a9de357c213025b07091e8b770064a6d7d6ff9
Plays Lokotama <lokotamathemastermind@gmail.com>
2020-08-22 14:33:22 +0100

    Start for compound interest calculator | Finished the area-of-square.py file

--- compound-interest-calc.py
+++ compound-interest-calc.py
@@ -1,8 +1,14 @@
-value_1 = input("Please what is the intial principal ->₦")
-value_2 = input("Please what is the rate ->%")
+value_1 = int(input("Please what is the intial principal ->₦"))
+value_2 = int(input("Please what is the rate ->%"))
 value_3 = int(input("How long do you want to watch the principal ->yrs"))
 
 def calculate(principal, rate, time):
-    pass
+    interest_1 = principal * rate * time / 100
+    how_long = time
+    temp = list(how_long)
+    print(temp)
+    # for var in how_long:
+    #     temp = principal * rate * time / 100
+    #     print(f"Interest for the year {var} is {temp}")
 
 calculate(value_1, value_2, value_3)
