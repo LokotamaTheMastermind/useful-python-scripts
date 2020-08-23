c814e7ab69d05bf715f5ea31d772a3b562f652a3
Plays Lokotama <lokotamathemastermind@gmail.com>
2020-08-23 10:06:32 +0100

    Added string reverser | Modified the simple-interest-calc.py file | Modified the compound-interest-calc.py file

--- compound-interest-calc.py
+++ compound-interest-calc.py
@@ -1,14 +1,21 @@
+print("")
 value_1 = int(input("Please what is the intial principal ->₦"))
+print("")
 value_2 = int(input("Please what is the rate ->%"))
+print("")
 value_3 = int(input("How long do you want to watch the principal ->yrs"))
+print("")
 
 def calculate(principal, rate, time):
-    interest_1 = principal * rate * time / 100
+    interest = principal * rate * time / 100
+
     how_long = time
-    temp = list(how_long)
-    print(temp)
-    # for var in how_long:
-    #     temp = principal * rate * time / 100
-    #     print(f"Interest for the year {var} is {temp}")
+
+    val = range(how_long + 1)[1:]
+
+    for var in val:
+        solution = interest + principal * rate * time / 100
+        solution += solution
+        print(f"\nInterest for the year {var} is {solution}")
 
 calculate(value_1, value_2, value_3)
--- simple-interest-calc.py
+++ simple-interest-calc.py
@@ -10,14 +10,32 @@ P * R * T
 """
 
 print("")
-value_1 = int(input("Please what is the principal ->₦"))
+value_1 = input("Please what is the principal ->₦")
+
+confirm_1 = value_1.isdigit()
+
+if confirm_1:
+    int(value_1)
+
 print("")
-value_2 = int(input("Please what is the rate ->%"))
+value_2 = input("Please what is the rate ->%")
+
+confirm_2 = value_2.isdigit()
+
+if confirm_2:
+    int(value_2)
+
 print("")
-value_3 = int(input("Please what is the time ->yrs"))
+value_3 = input("Please what is the time ->yrs")
+
+confirm_3 = value_3.isdigit()
+
+if confirm_3:
+    int(value_3)
 
 
 def calculate(principal, rate, time):
+
     print("")
 
     interest = principal * rate * time / 100
@@ -26,11 +44,47 @@ def calculate(principal, rate, time):
 
     question = input("Do you want to find the amount? |> ")
 
-    if question == "y" or "yes":
+    if question == "y" or question == "yes":
         amount = principal + interest
         print("")
         print(f"The amount of the simple interest is ->₦{amount}")
-    else:
-        print("")
 
-calculate(value_1, value_2, value_3)
+    another_question = input("Do you want the solution |> ")
+
+    if another_question == "yes" or another_question == "y":
+        show_solving(principal, rate, time)
+
+
+def show_solving(principal, rate, time):
+    name = """
+    Formula = P * R * T
+             -----------
+                  100
+    Interest = {} * {} * {}
+               ------------
+                   100
+    I =
+    """.format(principal, rate, time)
+    print(name)
+
+
+def show_solving_template():
+    import os
+    solving_template = """
+    Formula = P * R * T
+              ---------
+                  100
+
+    Interest = P * R * T
+               ---------
+                   100
+    """
+    command_1 = f"cd results"
+    os.system(command_1)
+
+
+if value_1 == "skip" or value_2 == "skip" or value_3 == "skip":
+    show_solving_template()
+
+else:
+    calculate(value_1, value_2, value_3)
