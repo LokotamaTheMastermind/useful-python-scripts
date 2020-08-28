b04e9cd986fe71cbb1d5f0ddd49d54e4b8b11fcc
Plays Lokotama <lokotamathemastermind@gmail.com>
2020-08-28 19:05:13 +0100

    Serious editing of helpful-stuff/git/project-manager.py | Dealing with configurations

--- helpful-stuff/git/project-manager.py
+++ helpful-stuff/git/project-manager.py
@@ -5,19 +5,34 @@ def main():
 
     if folder != "":
         if settings_dir_exists:
-            print("Available settings in the location")
-            print("Grabbing needed configuration")
-            settings_conf = open(f'{folder}/settings/configurations.txt', 'r')
-            lines = settings_conf.readlines()
-            print(lines)
+            os.system("cls")
+            configuration = open(f'{folder}/settings/configurations.txt', 'r')
+            content = configuration.readlines()
+            git_repository_published_line = content[0].strip()
+            process1 = git_repository_published_line.split("=")
+            final_git_repository_published = process1[1]
+
+            if final_git_repository_published == "True":
+                watch_published(configuration, folder)
+            else:
+                pass
+
         elif not settings_dir_exists:
             print("No settings found")
             print("Going through quick-setup of the configurations, please endure ...")
-        # var = lines[1].strip()
 
-
-def watch():
-    pass
-
+def watch_published(conf, settings_dir="./"):
+    import time
+    import os
+    print("Watching the repository")
+    while True:
+        settings_folder = os.path.exists(''.format(f"{settings_dir}/settings/"))
+        if settings_folder:
+            print("Refreshing github repository content (branch=master)")
+            remote_name = open(f'{settings_dir}/settings/configurations.txt')
+            content = remote_name.readlines()
+            print(content)
+        elif not settings_folder:
+            pass
 
 main()
