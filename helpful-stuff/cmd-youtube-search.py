9855e0fa2d018751ff4d09e57bed160cff617467
Plays Lokotama <lokotamathemastermind@gmail.com>
2020-08-29 01:33:24 +0100

    Extra modifications

--- helpful-stuff/cmd-youtube-search.py
+++ helpful-stuff/cmd-youtube-search.py
@@ -1,16 +1,34 @@
-search_query = input("What do you want to search on YouTube: ")
+try:
+    while True:
+        search_query = input("What do you want to search on YouTube: ")
+        def main(query):
+            if query != "":
+                import webbrowser
+                import time
+                import os
+                import platform
+                op == platform.system()
+                youtube_search = f"https://www.youtube.com/search?q={query}/"  # Constructed search query
+                formatted_youtube_search = youtube_search.replace(" ", "+")  # Formatted search query
+                print("\nYour search query has been saved and processed opening in YouTube")
+                time.sleep(3)
+                webbrowser.open(formatted_youtube_search)
+                if op == "Windows":
+                    os.system("cls")
+                elif op == "Linux" or op == "Darwin":
+                    os.system("clear")
+            else:
+                import os
+                import platform
+                op = platform.system()
+                if op == "Windows":
+                    os.system("cls")
+                elif op == "Linux" or op == "Darwin":
+                    os.system("clear")
+                else:
+                    print("Couldn't find system type")
+                print("\nSo can't search youtube for an empty string")
 
-def main(query):
-    if query != "":
-        import webbrowser
-        import time
-        youtube_search = f"https://www.youtube.com/search?q={query}/"
-        formatted_youtube_search = youtube_search.replace(" ", "+")
-        print("\nYour search query has been saved and processed opening in YouTube")
-        time.sleep(3)
-        webbrowser.open(formatted_youtube_search)
-    else:
-        print("So can't search google for an empty string")
-
-
-main(search_query)
+        main(search_query)
+except KeyboardInterrupt:
+    print("\nQuitting the console")
